7e096416977ca4796f60d1c40f35555e90f76e72
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-04 03:52:17 +0530

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-﻿import React from 'react'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
+import CartPage from './pages/CartPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product' element={<ProductDetails />} />
+        <Route path='/cart' element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
